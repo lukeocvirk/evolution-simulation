157fdba6335f9f250d7027ba318d7347313c0bb0
lukeocvirk <161389297+lukeocvirk@users.noreply.github.com>
2025-11-12 19:45:59 -0500

    Added simulation pause button (#6)
    * Pause button added -- not working yet
    
    * button now pauses properly.
    
    ---------
    
    Co-authored-by: Luke Ocvirk <luke.ocvirk@ibm.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,10 @@ import MoleculeField from "./MoleculeField";
 
 export default function App(): JSX.Element {
   return (
-    <main style={{ padding: 16, color: "#eaeaea", background: "#111", minHeight: "100vh" }}>
+    <main style={{ padding: 16, color: "#100958ff", background: "#d4d7fcff", minHeight: "100vh" }}>
       <h1 style={{ marginBottom: 8 }}>Evolution Simulator</h1>
       <p style={{ marginTop: 0, opacity: 0.8 }}>
-        React for UI, canvas for animation.
+        Project by Luke Ocvirk. React for UI, canvas for animation.
       </p>
       <MoleculeField />
     </main>
